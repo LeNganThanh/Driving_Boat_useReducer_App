1569acabb0e50a8eb5901f3a638cc773f9eca644
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-13 20:46:36 +0200

    modify some condition

---
+++ src/components/engineReducer.js
@@ -0,0 +1,123 @@
+//initial value
+export const initState = {
+  startState: false,
+  speed: 0,
+  gear: 0,
+  distance: 0,
+};
+//Reducer for execute the engine
+export const engineReducer = (state, action) => {
+  switch (action.type) {
+    //checking state if false then set true - the button for false is "Start" and when click "Start" then state turn to false and button is "Stop"
+    case "startStop": {
+      if (state.startState === false) {
+        return {
+          ...state,
+          startState: true,
+        };
+      } else {
+        return {
+          ...state,
+          startState: false,
+          gear: 0,
+        };
+      }
+    }
+
+    /**
+In case Engine is OFF (startState : false) then popup the Alert and return to start page 
+*/
+    //checking "gear" if less than 5 and state of Engine is ON(true) then can gearUp
+    case "gearUp": {
+      if (state.gear < 5 && state.startState === true) {
+        return {
+          ...state,
+          gear: state.gear + 1,
+        };
+      } else {
+        alert("Engine is OFF");
+        return { ...state };
+      }
+    }
+    //gear between -2 and 5 and state of Engine is ON can gearDown
+    case "gearDown": {
+      if (state.gear > -2 && state.gear < 5 && state.startState === true) {
+        return {
+          ...state,
+          gear: state.gear - 1,
+        };
+      } else {
+        alert("Engine is OFF");
+        return { ...state };
+      }
+    }
+    /* 
+1. In case the engine is OFF (startState: false) and gear = 0 then speed could not increase or decrease then popup Alert and return to start page 
+*/
+
+    /* 
+Distance is 5* speed :)
+Speed up has 4 cases:
+1. gear between 0 and 5 then normal speed up (+1)
+2. gear = 5 then the speed get each click (+5)
+3. gear = -1 then speed is normal (+1)
+4. gear = -2 the speed is getting faster (+5)
+*/
+    case "speedUp": {
+      if (state.gear > 0 && state.gear < 5 && state.startState === true) {
+        return {
+          ...state,
+          speed: state.speed + 1,
+          distance: state.speed * 5,
+        };
+      } else if (state.gear === 5 && state.startState === true) {
+        return {
+          ...state,
+          speed: state.speed + 5,
+          distance: state.speed * 5,
+        };
+      } else if (state.gear === -1 && state.startState === true) {
+        return {
+          ...state,
+          speed: state.speed + 1,
+          distance: state.speed * 5,
+        };
+      } else if (state.gear === -2 && state.startState === true) {
+        return {
+          ...state,
+          speed: state.speed + 5,
+          distance: state.speed * 5,
+        };
+      } else if (state.gear === 0) {
+        {
+          alert("Engine is OFF");
+          return { ...state };
+        }
+      }
+    }
+
+    //decrease the speed in case gear between 0 and 5 and check the speed is greater than 0 ( speed cannot be a minus value) if speed to 0 then set it to 0
+    case "speedDown": {
+      if (state.gear > 0 && state.gear <= 5 && state.startState === true) {
+        if (state.speed > 0) {
+          return {
+            ...state,
+            speed: state.speed - 5,
+          };
+        } else return { speed: 0 };
+      } else if (
+        state.gear === 0 ||
+        state.startState === false ||
+        state.speed <= 0
+      ) {
+        {
+          alert("Engine is OFF");
+          return { ...state };
+        }
+      }
+    }
+
+    default:
+      alert("Engine is broken 😢 ");
+  }
+};
